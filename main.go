99df72907877c6bfde55f0df8733b962cc7c5ae6
Полетанский Виктор <poletanskiy@kvado.ru>
2022-09-15 14:43:12 +0300

    dev: add pid saving

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 
 	"github.com/urfave/cli/v2"
 )
@@ -21,7 +22,7 @@ func main() {
 	var name string
 	runCmd := &cli.Command{
 		Name:      "start",
-		Aliases:   []string{"run", "r"},
+		Aliases:   []string{"run"},
 		ArgsUsage: "<cmd> [args...]",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
@@ -51,10 +52,20 @@ func main() {
 				return err
 			}
 
+			pidFile, err := os.OpenFile(path.Join(homeDir, name, "pid"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+			if err != nil {
+				return err
+			}
+
+			// TODO: syscall.ForkExec()
 			cmd := exec.CommandContext(ctx.Context, args[0], args[1:]...)
 			cmd.Stdout = stdoutLogFile
 			cmd.Stderr = stderrLogFile
 			if err := cmd.Start(); err != nil {
+				return err
+			}
+
+			if _, err := pidFile.WriteString(strconv.Itoa(cmd.Process.Pid)); err != nil {
 				return err
 			}
 
@@ -64,11 +75,31 @@ func main() {
 		},
 	}
 
+	listCmd := &cli.Command{
+		Name:    "list",
+		Aliases: []string{"l"},
+		Action: func(*cli.Context) error {
+			fs, err := os.ReadDir(homeDir)
+			if err != nil {
+				return err
+			}
+
+			for _, f := range fs {
+				if !f.IsDir() {
+					fmt.Fprintf(os.Stderr, "found strange file %q which should not exist\n", path.Join(homeDir, f.Name()))
+					continue
+				}
+				fmt.Printf("%#v", f.Name())
+			}
+			return nil
+		},
+	}
+
 	stopCmd := &cli.Command{
 		Name:      "stop",
 		Aliases:   []string{"kill", "s"},
 		ArgsUsage: "<name>",
-		Action: func(ctx *cli.Context) error {
+		Action: func(*cli.Context) error {
 			return nil
 		},
 	}
@@ -79,6 +110,7 @@ func main() {
 		Commands: []*cli.Command{
 			runCmd,
 			stopCmd,
+			listCmd,
 		},
 		Before: func(*cli.Context) error {
 			if _, err := os.Stat(homeDir); os.IsNotExist(err) {
