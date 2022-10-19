fe122cf1629d185aa038b37ae4b64fb031896b42
Poletansky Viktor <poletansky@gmail.com>
2022-10-20 00:28:21 +0300

    move out stop cmd

---
+++ cmd/internal/stop.go
@@ -0,0 +1,24 @@
+package internal
+
+import "github.com/urfave/cli/v2"
+
+var StopCmd = &cli.Command{
+	Name:      "stop",
+	Usage:     "stop a process",
+	ArgsUsage: "<id|name|namespace|all|json|stdin...>",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "watch",
+			Usage: "Stop watching folder for changes",
+		},
+	},
+	Action: func(*cli.Context) error {
+		//   .action(function(param) {
+		//     forEachLimit(param, 1, function(script, next) {
+		//       pm2.stop(script, next);
+		//     }, function(err) {
+		//       pm2.speedList(err ? 1 : 0);
+		//     });
+		return nil
+	},
+}
--- cmd/main.go
+++ cmd/main.go
@@ -38,27 +38,6 @@ func main() {
 		},
 	}
 
-	stopCmd := &cli.Command{
-		Name:      "stop",
-		Usage:     "stop a process",
-		ArgsUsage: "<id|name|namespace|all|json|stdin...>",
-		Flags: []cli.Flag{
-			&cli.BoolFlag{
-				Name:  "watch",
-				Usage: "Stop watching folder for changes",
-			},
-		},
-		Action: func(*cli.Context) error {
-			//   .action(function(param) {
-			//     forEachLimit(param, 1, function(script, next) {
-			//       pm2.stop(script, next);
-			//     }, function(err) {
-			//       pm2.speedList(err ? 1 : 0);
-			//     });
-			return nil
-		},
-	}
-
 	app := &cli.App{
 		Name:  "pm",
 		Usage: "manage running processes",
@@ -67,7 +46,7 @@ func main() {
 		},
 		Commands: []*cli.Command{
 			internal.StartCmd,
-			stopCmd,
+			internal.StopCmd,
 			listCmd,
 			internal.DaemonCmd,
 		},
