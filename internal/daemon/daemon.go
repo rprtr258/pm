49560fdcfbb71a15d9f50b0531df66cf50a13329
Poletansky Viktor <poletansky@gmail.com>
2022-10-23 16:06:01 +0300

    add delete

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -2,13 +2,10 @@ package daemon
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net"
 	"os"
-	"strconv"
 
 	"github.com/samber/lo"
 	"github.com/sevlyar/go-daemon"
@@ -21,198 +18,6 @@ import (
 
 	pb "github.com/rprtr258/pm/api"
 )
-
-const (
-	_mainBucket   = "main"
-	_byNameBucket = "by_name"
-	_byTagBucket  = "by_tag"
-)
-
-type ProcMetadata struct {
-	Name   string   `json:"name"`
-	Cmd    string   `json:"cmd"`
-	Status string   `json:"status"`
-	Tags   []string `json:"tags"`
-}
-
-type DB struct {
-	db bbolt.DB
-}
-
-func encodeUintKey(procID uint64) []byte {
-	return []byte(strconv.FormatUint(procID, 10))
-}
-
-func decodeUintKey(key []byte) (uint64, error) {
-	return strconv.ParseUint(string(key), 10, 64)
-}
-
-func encodeJSON[T any](proc T) ([]byte, error) {
-	return json.Marshal(proc)
-}
-
-func decodeJSON[T any](value []byte) (T, error) {
-	var res T
-	if err := json.Unmarshal(value, &res); err != nil {
-		return lo.Empty[T](), fmt.Errorf("failed decoding value of type %T: %w", res, err)
-	}
-
-	return res, nil
-}
-
-// TODO: template key type
-func get[V any](bucket *bbolt.Bucket, key []byte) (V, error) {
-	// keyBytes, err := encodeUintKey(key)
-	bytes := bucket.Get(key)
-	if bytes == nil {
-		return lo.Empty[V](), fmt.Errorf("value not found by key %v", key)
-	}
-
-	return decodeJSON[V](bytes)
-}
-
-// TODO: serialize/deserialize protobuffers
-// TODO: template key type
-func put[V any](bucket *bbolt.Bucket, key []byte, value V) error {
-	bytes, err := encodeJSON(value)
-	if err != nil {
-		return err
-	}
-
-	if err := bucket.Put(key, bytes); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (db *DB) AddTask(metadata ProcMetadata) (uint64, error) {
-	var procID uint64
-	if err := db.db.Update(func(tx *bbolt.Tx) error {
-		{
-			mainBucket := tx.Bucket([]byte(_mainBucket))
-			if mainBucket == nil {
-				return errors.New("main bucket was not found")
-			}
-
-			id, err := mainBucket.NextSequence()
-			if err != nil {
-				return err
-			}
-
-			procID = id
-
-			if err := put(mainBucket, encodeUintKey(id), metadata); err != nil {
-				return err
-			}
-		}
-		{
-			byNameBucket := tx.Bucket([]byte(_byNameBucket))
-			if byNameBucket == nil {
-				return errors.New("byName bucket was not found")
-			}
-
-			idsByName, err := get[[]uint64](byNameBucket, []byte(metadata.Name))
-			if err != nil {
-				return err
-			}
-
-			if err := put(byNameBucket, []byte(metadata.Name), append(idsByName, procID)); err != nil {
-				return err
-			}
-		}
-		{
-			byTagBucket := tx.Bucket([]byte(_byTagBucket))
-			if byTagBucket == nil {
-				return errors.New("byTag bucket was not found")
-			}
-
-			for _, tag := range metadata.Tags {
-				idsByTag, err := get[[]uint64](byTagBucket, []byte(tag))
-				if err != nil {
-					return err
-				}
-
-				if err := put(byTagBucket, []byte(metadata.Name), append(idsByTag, procID)); err != nil {
-					return err
-				}
-			}
-		}
-
-		return nil
-	}); err != nil {
-		return 0, err
-	}
-
-	return procID, nil
-}
-
-type ProcData struct {
-	ID       uint64
-	Metadata ProcMetadata
-}
-
-func (db *DB) List() ([]ProcData, error) {
-	var res []ProcData
-
-	if err := db.db.View(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket([]byte(_mainBucket))
-		if bucket == nil {
-			return errors.New("main bucket does not exist")
-		}
-
-		if err := bucket.ForEach(func(key, value []byte) error {
-			id, err := decodeUintKey(key)
-			if err != nil {
-				return fmt.Errorf("incorrect key: %w", err)
-			}
-
-			metadata, err := decodeJSON[ProcMetadata](value)
-			if err != nil {
-				return err
-			}
-
-			res = append(res, ProcData{
-				ID: id,
-				Metadata: ProcMetadata{
-					Name:   metadata.Name,
-					Cmd:    metadata.Cmd,
-					Status: metadata.Status,
-					Tags:   metadata.Tags,
-				},
-			})
-
-			return nil
-		}); err != nil {
-			return err
-		}
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return res, nil
-}
-
-func (db *DB) SetStatus(procID uint64, newStatus string /*TODO: enum statuses*/) error {
-	return db.db.Update(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket([]byte(_mainBucket))
-		if bucket == nil {
-			return errors.New("main bucket does not exist")
-		}
-
-		key := encodeUintKey(procID)
-		metadata, err := get[ProcMetadata](bucket, key)
-		if err != nil {
-			return err
-		}
-
-		metadata.Status = newStatus
-
-		return put(bucket, key, metadata)
-	})
-}
 
 type daemonServer struct {
 	pb.UnimplementedDaemonServer
@@ -329,16 +134,23 @@ func (srv *daemonServer) List(context.Context, *emptypb.Empty) (*pb.ListResp, er
 	}, nil
 }
 
-func (srv *daemonServer) Stop(_ context.Context, req *pb.DeleteReq) (*pb.DeleteResp, error) {
-	procs, err := srv.DB.List()
+func filterProcs(db *DB) ([]ProcData, error) {
+	procs, err := db.List()
 	if err != nil {
 		return nil, status.Errorf(codes.DataLoss, err.Error())
 	}
 
-	procsToStop := lo.Filter(procs, func(proc ProcData, _ int) bool {
+	return lo.Filter(procs, func(proc ProcData, _ int) bool {
 		// TODO: filter based on req.GetFilters()
 		return true
-	})
+	}), nil
+}
+
+func (srv *daemonServer) Stop(_ context.Context, req *pb.DeleteReq) (*pb.DeleteResp, error) {
+	procsToStop, err := filterProcs(srv.DB)
+	if err != nil {
+		return nil, status.Errorf(codes.DataLoss, err.Error())
+	}
 
 	for _, proc := range procsToStop {
 		// TODO: stop proc
@@ -354,8 +166,23 @@ func (srv *daemonServer) Stop(_ context.Context, req *pb.DeleteReq) (*pb.DeleteR
 	}, nil
 }
 
-func (*daemonServer) Delete(_ context.Context, req *pb.DeleteReq) (*pb.DeleteResp, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
+func (srv *daemonServer) Delete(_ context.Context, req *pb.DeleteReq) (*pb.DeleteResp, error) {
+	procsToDelete, err := filterProcs(srv.DB)
+	if err != nil {
+		return nil, status.Errorf(codes.DataLoss, err.Error())
+	}
+
+	for _, proc := range procsToDelete {
+		if err := srv.DB.Delete(proc.ID); err != nil {
+			return nil, err
+		}
+	}
+
+	return &pb.DeleteResp{
+		Id: lo.Map(procsToDelete, func(proc ProcData, _ int) uint64 {
+			return proc.ID
+		}),
+	}, nil
 }
 
 // Run daemon
---
+++ internal/daemon/db.go
@@ -0,0 +1,255 @@
+package daemon
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/samber/lo"
+	"go.etcd.io/bbolt"
+)
+
+const (
+	_mainBucket   = "main"
+	_byNameBucket = "by_name"
+	_byTagBucket  = "by_tag"
+)
+
+type ProcMetadata struct {
+	Name   string   `json:"name"`
+	Cmd    string   `json:"cmd"`
+	Status string   `json:"status"`
+	Tags   []string `json:"tags"`
+}
+
+type ProcData struct {
+	ID       uint64
+	Metadata ProcMetadata
+}
+
+type DB struct {
+	db bbolt.DB
+}
+
+func encodeUintKey(procID uint64) []byte {
+	return []byte(strconv.FormatUint(procID, 10))
+}
+
+func decodeUintKey(key []byte) (uint64, error) {
+	return strconv.ParseUint(string(key), 10, 64)
+}
+
+func encodeJSON[T any](proc T) ([]byte, error) {
+	return json.Marshal(proc)
+}
+
+func decodeJSON[T any](value []byte) (T, error) {
+	var res T
+	if err := json.Unmarshal(value, &res); err != nil {
+		return lo.Empty[T](), fmt.Errorf("failed decoding value of type %T: %w", res, err)
+	}
+
+	return res, nil
+}
+
+// TODO: template key type
+func get[V any](bucket *bbolt.Bucket, key []byte) (V, error) {
+	// keyBytes, err := encodeUintKey(key)
+	bytes := bucket.Get(key)
+	if bytes == nil {
+		return lo.Empty[V](), fmt.Errorf("value not found by key %v", key)
+	}
+
+	return decodeJSON[V](bytes)
+}
+
+// TODO: serialize/deserialize protobuffers
+// TODO: template key type
+func put[V any](bucket *bbolt.Bucket, key []byte, value V) error {
+	bytes, err := encodeJSON(value)
+	if err != nil {
+		return err
+	}
+
+	if err := bucket.Put(key, bytes); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB) AddTask(metadata ProcMetadata) (uint64, error) {
+	var procID uint64
+	if err := db.db.Update(func(tx *bbolt.Tx) error {
+		{
+			mainBucket := tx.Bucket([]byte(_mainBucket))
+			if mainBucket == nil {
+				return errors.New("main bucket was not found")
+			}
+
+			id, err := mainBucket.NextSequence()
+			if err != nil {
+				return err
+			}
+
+			procID = id
+
+			if err := put(mainBucket, encodeUintKey(id), metadata); err != nil {
+				return err
+			}
+		}
+		{
+			byNameBucket := tx.Bucket([]byte(_byNameBucket))
+			if byNameBucket == nil {
+				return errors.New("byName bucket was not found")
+			}
+
+			idsByName, err := get[[]uint64](byNameBucket, []byte(metadata.Name))
+			if err != nil {
+				return err
+			}
+
+			if err := put(byNameBucket, []byte(metadata.Name), append(idsByName, procID)); err != nil {
+				return err
+			}
+		}
+		{
+			byTagBucket := tx.Bucket([]byte(_byTagBucket))
+			if byTagBucket == nil {
+				return errors.New("byTag bucket was not found")
+			}
+
+			for _, tag := range metadata.Tags {
+				idsByTag, err := get[[]uint64](byTagBucket, []byte(tag))
+				if err != nil {
+					return err
+				}
+
+				if err := put(byTagBucket, []byte(tag), append(idsByTag, procID)); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return 0, err
+	}
+
+	return procID, nil
+}
+
+func (db *DB) List() ([]ProcData, error) {
+	var res []ProcData
+
+	if err := db.db.View(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte(_mainBucket))
+		if bucket == nil {
+			return errors.New("main bucket does not exist")
+		}
+
+		if err := bucket.ForEach(func(key, value []byte) error {
+			id, err := decodeUintKey(key)
+			if err != nil {
+				return fmt.Errorf("incorrect key: %w", err)
+			}
+
+			metadata, err := decodeJSON[ProcMetadata](value)
+			if err != nil {
+				return err
+			}
+
+			res = append(res, ProcData{
+				ID: id,
+				Metadata: ProcMetadata{
+					Name:   metadata.Name,
+					Cmd:    metadata.Cmd,
+					Status: metadata.Status,
+					Tags:   metadata.Tags,
+				},
+			})
+
+			return nil
+		}); err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (db *DB) SetStatus(procID uint64, newStatus string /*TODO: enum statuses*/) error {
+	return db.db.Update(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte(_mainBucket))
+		if bucket == nil {
+			return errors.New("main bucket does not exist")
+		}
+
+		key := encodeUintKey(procID)
+		metadata, err := get[ProcMetadata](bucket, key)
+		if err != nil {
+			return err
+		}
+
+		metadata.Status = newStatus
+
+		return put(bucket, key, metadata)
+	})
+}
+
+func (db *DB) Delete(procID uint64) error {
+	return db.db.Update(func(tx *bbolt.Tx) error {
+		mainBucket := tx.Bucket([]byte(_mainBucket))
+		if mainBucket == nil {
+			return errors.New("main bucket was not found")
+		}
+
+		key := encodeUintKey(procID)
+
+		proc, err := get[ProcMetadata](mainBucket, key)
+		if err != nil {
+			return err
+		}
+
+		if err := mainBucket.Delete(key); err != nil {
+			return err
+		}
+
+		{
+			byNameBucket := tx.Bucket([]byte(_byNameBucket))
+			if byNameBucket == nil {
+				return errors.New("byName bucket was not found")
+			}
+
+			if err := byNameBucket.Delete([]byte(proc.Name)); err != nil {
+				return err
+			}
+		}
+		{
+			byTagBucket := tx.Bucket([]byte(_byTagBucket))
+			if byTagBucket == nil {
+				return errors.New("byTag bucket was not found")
+			}
+
+			for _, tag := range proc.Tags {
+				idsByTag, err := get[[]uint64](byTagBucket, []byte(tag))
+				if err != nil {
+					return err
+				}
+
+				if err := put(byTagBucket, []byte(tag), lo.Reject(idsByTag, func(item uint64, _ int) bool {
+					return item == procID
+				})); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+}
