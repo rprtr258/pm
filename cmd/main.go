5fd72ea50faa1c1368182dd16b7477c45e11d9e9
Poletansky Viktor <poletansky@gmail.com>
2022-11-06 15:41:38 +0300

    abobus

--- cmd/main.go
+++ cmd/main.go
@@ -47,7 +47,7 @@ func main() {
 		Before: func(*cli.Context) error {
 			// TODO: run daemon if not running
 			if _, err := os.Stat(internal.HomeDir); os.IsNotExist(err) {
-				os.Mkdir(internal.HomeDir, 0755)
+				return os.Mkdir(internal.HomeDir, 0755)
 			}
 
 			return nil
