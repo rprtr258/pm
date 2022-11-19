e9b1d3f3b9872f1f633426ae8001a193b221350b
Poletansky Viktor <poletansky@gmail.com>
2022-11-19 16:19:06 +0300

    naming

--- internal/util.go
+++ internal/util.go
@@ -12,18 +12,18 @@ func MapDict[T comparable, R any](collection []T, dict map[T]R) []R {
 	return result
 }
 
-func IfNotNil[T, R any](ptr *T, call func(T) R) R {
+func IfNotNil[T, R any](ptr *T, mapper func(T) R) R {
 	if ptr == nil {
 		return lo.Empty[R]()
 	}
-	return call(*ptr)
+	return mapper(*ptr)
 }
 
-func FilterMapToSlice[K comparable, V, R any](in map[K]V, iteratee func(key K, value V) (R, bool)) []R {
+func FilterMapToSlice[K comparable, V, R any](in map[K]V, mapper func(key K, value V) (R, bool)) []R {
 	result := make([]R, 0, len(in))
 
 	for k, v := range in {
-		y, ok := iteratee(k, v)
+		y, ok := mapper(k, v)
 		if !ok {
 			continue
 		}
