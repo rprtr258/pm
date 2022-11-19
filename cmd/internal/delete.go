7c7f465cdaf4416e6cb8d9b0177694a8d1a854bc
Poletansky Viktor <poletansky@gmail.com>
2022-11-19 20:01:03 +0300

    add filters to delete cmd

--- cmd/internal/delete.go
+++ cmd/internal/delete.go
@@ -1,12 +1,13 @@
 package internal
 
 import (
-	"errors"
+	"context"
 	"fmt"
-	"strconv"
 
 	"github.com/rprtr258/pm/api"
+	"github.com/rprtr258/pm/internal"
 	"github.com/rprtr258/pm/internal/db"
+	"github.com/samber/lo"
 	"github.com/urfave/cli/v2"
 )
 
@@ -19,46 +20,72 @@ var DeleteCmd = &cli.Command{
 	Aliases:   []string{"del", "rm"},
 	Usage:     "stop and remove process",
 	ArgsUsage: "<name|id|namespace|tag|json>...",
-	Action: func(ctx *cli.Context) error {
-		client, deferFunc, err := NewGrpcClient()
-		if err != nil {
-			return err
-		}
-		defer deferErr(deferFunc)
-
-		args := ctx.Args().Slice()
-		if len(args) < 1 {
-			return errors.New("what to delete expected")
-		}
-
-		procIDs := []uint64{} // TODO: implement
-
-		for _, arg := range args {
-			procID, err := strconv.ParseUint(arg, 10, 64)
-			if err != nil {
-				return fmt.Errorf("%q is not uint: %w", arg, err)
-			}
-
-			procIDs = append(procIDs, procID)
-		}
-
-		if _, err := client.Stop(ctx.Context, &api.IDs{Ids: procIDs}); err != nil {
-			return fmt.Errorf("client.Stop failed: %w", err)
-		}
-
-		if err := db.New(_daemonDBFile).Delete(procIDs); err != nil {
-			return err // TODO: add errs descriptions, loggings
-		}
-
-		// TODO: delete log files too
-		// 	_, err := os.Stat(string(*f))
-		// 	if err != nil {
-		// 		return true
-		// 	}
-		// 	err = os.Remove(string(*f))
-		// 	return err == nil
-		// }
-
-		return nil
+	Flags: []cli.Flag{
+		&cli.StringSliceFlag{
+			Name:  "name",
+			Usage: "name(s) of process(es) to stop",
+		},
+		&cli.StringSliceFlag{
+			Name:  "tag",
+			Usage: "tag(s) of process(es) to stop",
+		},
+		&cli.Uint64SliceFlag{
+			Name:  "id",
+			Usage: "id(s) of process(es) to stop",
+		},
 	},
+	Action: func(ctx *cli.Context) error {
+		return delete(
+			ctx.Context,
+			ctx.Args().Slice(),
+			ctx.StringSlice("name"),
+			ctx.StringSlice("tags"),
+			ctx.Uint64Slice("id"),
+		)
+	},
+}
+
+func delete(
+	ctx context.Context,
+	genericFilters, nameFilters, tagFilters []string,
+	idFilters []uint64,
+) error {
+	client, deferFunc, err := NewGrpcClient()
+	if err != nil {
+		return err
+	}
+	defer deferErr(deferFunc)
+
+	resp, err := db.New(_daemonDBFile).List()
+	if err != nil {
+		return err
+	}
+
+	procIDs := lo.Map(internal.FilterProcs(
+		resp,
+		internal.WithGeneric(genericFilters),
+		internal.WithIDs(idFilters),
+		internal.WithNames(nameFilters),
+		internal.WithTags(tagFilters),
+	), func(id db.ProcID, _ int) uint64 {
+		return uint64(id)
+	})
+
+	if _, err := client.Stop(ctx, &api.IDs{Ids: procIDs}); err != nil {
+		return fmt.Errorf("client.Stop failed: %w", err)
+	}
+
+	if err := db.New(_daemonDBFile).Delete(procIDs); err != nil {
+		return err // TODO: add errs descriptions, loggings
+	}
+
+	// TODO: delete log files too
+	// 	_, err := os.Stat(string(*f))
+	// 	if err != nil {
+	// 		return true
+	// 	}
+	// 	err = os.Remove(string(*f))
+
+	return nil
+
 }
