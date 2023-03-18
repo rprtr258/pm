0892e0ec03ee0079fa11f9803c74fcbf8b801a62
Poletansky Viktor <poletansky@gmail.com>
2023-03-19 01:02:46 +0300

    dev: u

--- cmd/main.go
+++ cmd/main.go
@@ -16,18 +16,19 @@ func main() {
 	app := &cli.App{
 		Name:  "pm",
 		Usage: "manage running processes",
-		// If sets and script’s memory usage goes about the configured number, pm2 restarts the script. Uses human-friendly suffixes: ‘K’ for kilobytes, ‘M’ for megabytes, ‘G’ for gigabytes’, etc. Eg “150M”.  // &cli.IntFlag{Name: "max-memory-restart", Usage: "Restart the app if an amount of memory is exceeded (in bytes)"},
 		Flags: []cli.Flag{
+			// If sets and script’s memory usage goes about the configured number, pm2 restarts the script.
+			// Uses human-friendly suffixes: ‘K’ for kilobytes, ‘M’ for megabytes, ‘G’ for gigabytes’, etc. Eg “150M”.
+			// &cli.IntFlag{Name: "max-memory-restart", Usage: "Restart the app if an amount of memory is exceeded (in bytes)"},
 			// &cli.BoolFlag{Name:        "attach", Usage: "attach logging after your start/restart/stop/reload"},
-			// &cli.BoolFlag{Name:        "disable-trace", Usage: "disable transaction tracing with km"},
 			// &cli.DurationFlag{Name:    "listen-timeout", Usage: "listen timeout on application reload"},
 			// &cli.BoolFlag{Name:        "no-color", Usage: "skip colors"},
 			// &cli.BoolFlag{Name:        "no-daemon", Usage: "run pm2 daemon in the foreground if it doesn\t exist already"},
 			// &cli.BoolFlag{Name:        "no-vizion", Usage: "start an app without vizion feature (versioning control)"},
 			// &cli.IntFlag{Name:         "parallel", Usage: "number of parallel actions (for restart/reload)"},
 			// &cli.BoolFlag{Name:        "silent", Aliases: []string{"s"}, Usage: "hide all messages", Value: false},
-			// &cli.BoolFlag{Name:        "trace", Usage: "enable transaction tracing with km"},
-			// &cli.BoolFlag{Name:        "wait-ip", Usage: "override systemd script to wait for full internet connectivity to launch pm2"},
+			// &cli.BoolFlag{Name:        "wait-ip",
+			//               Usage: "override systemd script to wait for full internet connectivity to launch pm2"},
 		},
 		Commands: append(
 			internal.AllCmds,
