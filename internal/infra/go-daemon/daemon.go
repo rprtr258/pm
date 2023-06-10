83561e9ac3d33bd7e09c11b301a57d59f7f5f0d7
Poletansky Viktor <poletansky@gmail.com>
2023-06-10 16:51:57 +0300

    dev: u

--- internal/infra/go-daemon/daemon.go
+++ internal/infra/go-daemon/daemon.go
@@ -6,7 +6,9 @@ import (
 	"path/filepath"
 	"syscall"
 
+	"github.com/rprtr258/log"
 	"github.com/rprtr258/xerr"
+	"github.com/samber/lo"
 	"golang.org/x/sys/unix"
 )
 
@@ -61,8 +63,8 @@ type Context struct {
 	Umask int
 }
 
-// WasReborn returns true in child process (daemon) and false in parent process.
-func WasReborn() bool {
+// AmIDaemon returns true in child process (daemon) and false in parent process.
+func AmIDaemon() bool {
 	return os.Getenv(MarkName) == MarkValue
 }
 
@@ -73,20 +75,11 @@ func WasReborn() bool {
 // In success returns *os.Process in parent process and nil in child process.
 // Otherwise returns error.
 func (d *Context) Reborn() (*os.Process, error) {
-	if WasReborn() {
-		err := d.child()
-		if err != nil {
-			return nil, xerr.NewWM(err, "reborn child failed")
-		}
-		return nil, nil
+	if !AmIDaemon() {
+		return d.parent()
 	}
 
-	child, err := d.parent()
-	if err != nil {
-		return nil, xerr.NewWM(err, "reborn parent failed")
-	}
-
-	return child, nil
+	return nil, d.child()
 }
 
 // Search searches daemons process by given in context pid file name.
@@ -94,14 +87,13 @@ func (d *Context) Reborn() (*os.Process, error) {
 // else returns error. Returns nil if filename is empty.
 func (d *Context) Search() (*os.Process, error) {
 	if len(d.PidFileName) == 0 {
-		return nil, nil
+		return nil, xerr.NewM("pidFileName is empty")
 	}
 
 	pid, err := ReadPidFile(d.PidFileName)
 	if err != nil {
 		if _, ok := xerr.As[*PidFileNotFoundError](err); ok {
-			// TODO: ???
-			return nil, nil
+			return nil, xerr.NewWM(err, "pid file not found", xerr.Fields{"pidFileName": d.PidFileName})
 		}
 
 		return nil, xerr.NewWM(err, "read pid file", xerr.Fields{"pidFileName": d.PidFileName})
@@ -115,13 +107,13 @@ func (d *Context) Search() (*os.Process, error) {
 	}
 
 	if daemon == nil {
-		return nil, nil
+		return nil, xerr.NewM("daemon not found", xerr.Fields{"pid": pid})
 	}
 
 	// Send a test signal to test if this daemon is actually alive or dead.
 	if err := daemon.Signal(syscall.Signal(0)); err != nil {
 		// An error means it is dead.
-		return nil, nil
+		return nil, xerr.NewWM(err, "check daemon", xerr.Fields{"pid": pid})
 	}
 
 	return daemon, nil
@@ -144,7 +136,11 @@ func (d *Context) parent() (*os.Process, error) {
 	if err := d.openFiles(); err != nil {
 		return nil, err
 	}
-	defer d.closeFiles()
+	defer func() {
+		if errClose := d.closeFiles(); errClose != nil {
+			log.Error(errClose.Error())
+		}
+	}()
 
 	attr := &os.ProcAttr{
 		Dir:   d.WorkDir,
@@ -186,7 +182,7 @@ func (d *Context) openFiles() error {
 		return xerr.NewWM(err, "open /dev/null")
 	}
 
-	if len(d.PidFileName) > 0 {
+	if len(d.PidFileName) > 0 { //nolint:nestif // ???
 		if d.PidFileName, err = filepath.Abs(d.PidFileName); err != nil {
 			return xerr.NewWM(err, "filepath.Abs", xerr.Fields{"pidFilename": d.PidFileName})
 		}
@@ -212,12 +208,17 @@ func (d *Context) openFiles() error {
 		}
 	}
 
-	if len(d.LogFileName) > 0 {
-		if d.LogFileName == "/dev/stdout" {
-			d.logFile = os.Stdout
-		} else if d.LogFileName == "/dev/stderr" {
-			d.logFile = os.Stderr
-		} else if d.logFile, err = os.OpenFile(
+	if len(d.LogFileName) == 0 {
+		return nil
+	}
+
+	switch d.LogFileName {
+	case "/dev/stdout":
+		d.logFile = os.Stdout
+	case "/dev/stderr":
+		d.logFile = os.Stderr
+	default:
+		if d.logFile, err = os.OpenFile(
 			d.LogFileName,
 			os.O_WRONLY|os.O_CREATE|os.O_APPEND,
 			d.LogFilePerm,
@@ -233,17 +234,22 @@ func (d *Context) openFiles() error {
 }
 
 func (d *Context) closeFiles() error {
-	close := func(file **os.File) {
-		if *file != nil {
-			(*file).Close()
-			*file = nil
+	closeAndEmpty := func(file **os.File) error {
+		if *file == nil {
+			return nil
 		}
-	}
-	close(&d.logFile)
-	close(&d.nullFile)
-	close(&d.pidFile.File)
 
-	return nil
+		errClose := (*file).Close()
+		*file = nil
+
+		return xerr.NewWM(errClose, "close file", xerr.Fields{"filename": (*file).Name()})
+	}
+
+	return xerr.Combine(
+		closeAndEmpty(&d.logFile),
+		closeAndEmpty(&d.nullFile),
+		closeAndEmpty(&d.pidFile.File),
+	)
 }
 
 func (d *Context) prepareEnv() error {
@@ -266,13 +272,10 @@ func (d *Context) prepareEnv() error {
 }
 
 func (d *Context) files() []*os.File {
-	log := d.nullFile
-	if d.logFile != nil {
-		log = d.logFile // TODO: just log=d.logFile ?
-	}
+	log := lo.If(d.logFile != nil, d.logFile).Else(d.nullFile)
 
 	files := []*os.File{
-		d.nullFile, // d.rpipe,    // (0) stdin
+		d.nullFile, // (0) stdin
 		log,        // (1) stdout
 		log,        // (2) stderr
 		d.nullFile, // (3) dup on fd 0 after initialization
@@ -291,11 +294,12 @@ func (d *Context) child() (err error) {
 	if initialized {
 		return os.ErrInvalid
 	}
+
 	initialized = true
 
 	// create PID file after context decoding to know PID file full path.
 	if len(d.PidFileName) > 0 {
-		d.pidFile = NewLockFile(os.NewFile(4, d.PidFileName))
+		d.pidFile = NewLockFile(os.NewFile(4, d.PidFileName)) //nolint:gomnd // ???
 		if errWritePid := d.pidFile.WritePid(); errWritePid != nil {
 			return errWritePid
 		}
@@ -306,7 +310,7 @@ func (d *Context) child() (err error) {
 		}()
 	}
 
-	if errDup := unix.Dup2(3, 0); errDup != nil {
+	if errDup := unix.Dup2(3, 0); errDup != nil { //nolint:gomnd // ???
 		return xerr.NewWM(errDup, "Dup2")
 	}
 
