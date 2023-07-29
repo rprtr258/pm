bbe88234dafaf228c711bba42b6952bdbea1f900
Poletansky Viktor <poletansky@gmail.com>
2023-07-29 08:40:22 +0300

    cook: rename filterConfig to filter

--- internal/core/filter.go
+++ internal/core/filter.go
@@ -9,14 +9,14 @@ import (
 	"github.com/rprtr258/pm/internal/core/fun"
 )
 
-type filterConfig struct {
+type filter struct {
 	Names          []string
 	Tags           []string
 	IDs            []ProcID
 	allIfNoFilters bool
 }
 
-type FilterOption func(*filterConfig)
+type FilterOption func(*filter)
 
 func WithGeneric(args []string) FilterOption {
 	ids := lo.FilterMap(args, func(id string, _ int) (ProcID, bool) {
@@ -24,7 +24,7 @@ func WithGeneric(args []string) FilterOption {
 		return ProcID(procID), err == nil
 	})
 
-	return func(cfg *filterConfig) {
+	return func(cfg *filter) {
 		cfg.IDs = append(cfg.IDs, ids...)
 		cfg.Names = append(cfg.Names, args...)
 		cfg.Tags = append(cfg.Tags, args...)
@@ -32,19 +32,19 @@ func WithGeneric(args []string) FilterOption {
 }
 
 func WithNames(names []string) FilterOption {
-	return func(cfg *filterConfig) {
+	return func(cfg *filter) {
 		cfg.Names = append(cfg.Names, names...)
 	}
 }
 
 func WithTags(tags []string) FilterOption {
-	return func(cfg *filterConfig) {
+	return func(cfg *filter) {
 		cfg.Tags = append(cfg.Tags, tags...)
 	}
 }
 
 func WithIDs(ids []uint64) FilterOption {
-	return func(cfg *filterConfig) {
+	return func(cfg *filter) {
 		procIDs := fun2.Map(ids, func(id uint64) ProcID {
 			return ProcID(id)
 		})
@@ -52,32 +52,31 @@ func WithIDs(ids []uint64) FilterOption {
 	}
 }
 
-func WithAllIfNoFilters(cfg *filterConfig) {
+func WithAllIfNoFilters(cfg *filter) {
 	cfg.allIfNoFilters = true
 }
 
 func FilterProcs[T ~uint64](procs map[ProcID]Proc, opts ...FilterOption) []T {
-	var cfg filterConfig
+	var cfg filter
 	for _, opt := range opts {
 		opt(&cfg)
 	}
 
-	// if no filters, return all if allIfNoFilter, nothing otherwise
-	if len(cfg.Names) == 0 &&
+	noFilters := len(cfg.Names) == 0 &&
 		len(cfg.Tags) == 0 &&
-		len(cfg.IDs) == 0 {
-		if !cfg.allIfNoFilters {
-			return nil
-		}
-
+		len(cfg.IDs) == 0
+	switch {
+	case !noFilters:
+		return fun.FilterMapToSlice(procs, func(procID ProcID, proc Proc) (T, bool) {
+			return T(procID), lo.Contains(cfg.Names, proc.Name) ||
+				lo.Some(cfg.Tags, proc.Tags) ||
+				lo.Contains(cfg.IDs, proc.ID)
+		})
+	case cfg.allIfNoFilters:
 		return lo.Map(lo.Keys(procs), func(id ProcID, _ int) T {
 			return T(id)
 		})
+	default:
+		return nil
 	}
-
-	return fun.FilterMapToSlice(procs, func(procID ProcID, proc Proc) (T, bool) {
-		return T(procID), lo.Contains(cfg.Names, proc.Name) ||
-			lo.Some(cfg.Tags, proc.Tags) ||
-			lo.Contains(cfg.IDs, proc.ID)
-	})
 }
