6829723d304a5828a691aaefec7be72a8778443d
Poletansky Viktor <poletansky@gmail.com>
2024-04-26 03:29:46 +0300

    [cook] inline logger

--- main.go
+++ main.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"os"
+	"strings"
+	"time"
 
+	"github.com/rprtr258/scuf"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
+	"github.com/rprtr258/fun"
 	"github.com/rprtr258/pm/internal/infra/cli"
-	mylog "github.com/rprtr258/pm/internal/infra/log"
 )
 
+func newLogger() zerolog.Logger {
+	return zerolog.New(os.Stderr).With().
+		Timestamp().
+		Caller().
+		Logger().
+		Output(zerolog.ConsoleWriter{
+			Out: os.Stderr,
+			FormatLevel: func(i interface{}) string {
+				s := i.(string)
+				bg := fun.Switch(s, scuf.BgRed).
+					Case(scuf.BgBlue, zerolog.LevelInfoValue).
+					Case(scuf.BgGreen, zerolog.LevelWarnValue).
+					Case(scuf.BgYellow, zerolog.LevelErrorValue).
+					End()
+
+				return scuf.String(" "+strings.ToUpper(s)+" ", bg, scuf.FgBlack)
+			},
+			FormatTimestamp: func(i interface{}) string {
+				s := i.(string)
+				t, err := time.Parse(zerolog.TimeFieldFormat, s)
+				if err != nil {
+					return s
+				}
+
+				return scuf.String(t.Format("[15:06:05]"), scuf.ModFaint, scuf.FgWhite)
+			},
+		})
+}
+
 func main() {
-	log.Logger = mylog.New()
+	log.Logger = newLogger()
 
 	if err := cli.Run(os.Args); err != nil {
 		_ = err // NOTE: ignore, since cobra will print the error
