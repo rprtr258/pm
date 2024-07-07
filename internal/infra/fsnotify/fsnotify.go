6ee7faba44e6aeac44408e193b4518b8b7178a45
Poletansky Viktor <poletansky@gmail.com>
2024-07-07 23:59:45 +0300

    [cook] u

--- internal/infra/fsnotify/fsnotify.go
+++ internal/infra/fsnotify/fsnotify.go
@@ -3,17 +3,15 @@
 // for batching events.
 package fsnotify
 
-import (
-	"github.com/fsnotify/fsnotify"
-)
+import "github.com/fsnotify/fsnotify"
 
 // Provide access to the underlying fsnotify ops.
 const (
-	Chmod  = fsnotify.Chmod
 	Create = fsnotify.Create
+	Write  = fsnotify.Write
 	Remove = fsnotify.Remove
 	Rename = fsnotify.Rename
-	Write  = fsnotify.Write
+	Chmod  = fsnotify.Chmod
 )
 
 type Event = fsnotify.Event
